13f4fde408fb85fb249d4c2b7ae17f4b27014d9f
Adriaan Knapen <a.d.knapen@protonmail.com>
2018-12-09 13:34:27 +0200

    new: added day_9_2; new: significant performance increase (running time reduced from O(n^2) to O(n)

--- day9/main.go
+++ day9/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"container/ring"
+	"./ring"
 	"fmt"
+	"gopkg.in/cheggaaa/pb.v1"
 	"math"
 )
 
@@ -22,28 +23,37 @@ func printRing(r *ring.Ring) {
 }
 
 func main() {
-	fmt.Println("Part 1 example:", part1(9, 25), "(", 32, ")")
-	fmt.Println("Part 1 demo 1:", part1(10, 1618), "(", 8317, ")")
-	fmt.Println("Part 1 demo 5:", part1(30, 5807), "(", 37305, ")")
-	fmt.Println("Part 1:", part1(459, 71790))
+	fmt.Println("Part 1 example:", playGame(9, 25), "(", 32, ")")
+	fmt.Println("Part 1 demo 1:", playGame(10, 1618), "(", 8317, ")")
+	fmt.Println("Part 1 demo 5:", playGame(30, 5807), "(", 37305, ")")
+	fmt.Println("Part 1:", playGame(459, 71790))
+
+	fmt.Println("Part 2:", playGame(459, 71790 * 100))
+
 }
 
-func part1(playerCount int, lastMarbleValue int) int {
+func playGame(playerCount int, lastMarbleValue int) int {
 	score := map[int]int{}
 	game := ring.New(1)
 	game.Value = 0
 
+	bar := pb.StartNew(lastMarbleValue)
+	bar.SetWidth(80)
+
 	for i := 1; i <= lastMarbleValue; i++ {
+		bar.Increment()
 		if i % 23 == 0 {
 			game = game.Move(-8)
 			score[i % playerCount] += i + game.Value.(int) // Update the score for the current player
-			game = game.Unlink(game.Len()-1).Move(1) // Remove all but the previous marble and continue with the next one.
+			game = game.Pop().Move(1) // Remove the previous marble and continue with the next one.
 		} else {
 			marble := ring.New(1)
 			marble.Value = i
 			game = game.Link(marble)
 		}
 	}
+
+	bar.Finish()
 
 	return max(score)
 }
